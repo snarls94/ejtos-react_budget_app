362239e53697f9530ed5d3f3e63e8b0f4c823279
“Sruthi <sruthinarlaa@gmail.com>
2023-06-16 17:17:03 -0400

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,18 @@
 import React, {useContext} from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const {budget} = useContext(AppContext);
+    const {budget, dispatch} = useContext(AppContext);
+    const handleBudgetChange = (e) => {
+        if (e.target.value <= 20000) {
+            dispatch({ type: 'SET_BUDGET', payload: parseInt(e.target.value) });
+        } else {
+            alert('Budget cannot exceed 20,000');
+        }
+    };
     return(
         <div className = 'alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input type="number" min="0" max="20000" step="10" value={budget} onChange={handleBudgetChange}/>
         </div>
     );
 };
